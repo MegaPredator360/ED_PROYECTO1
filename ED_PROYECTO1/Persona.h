faeac634af550efb8022b0194a58de72948c0960
MegaPredator360 <aaronalfarozamora@gmail.com>
2023-06-03 20:54:04 -0600

    Agregando archivos

---
+++ ED_PROYECTO1/Persona.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Persona
+{
+// Valores a usar en la lista de empleados
+private:
+	string Cedula;
+	string Nombre;
+	string Nacionalidad;
+	string Residencia;
+	int Telefono;
+	int NumeroHijos;
+	string EstadoCivil;
+	int HorasTrabajo;
+
+public:
+	// Constructores n
+	Persona(void);
+	Persona(
+		string perCedula,
+		string perNombre,
+		string perNacionalidad,
+		string perResidencia,
+		int perTelefono,
+		int perNumeroHijos,
+		string perEstadoCivil,
+		int perHorasTrabajo
+	);
+
+	// Getters
+	string getCedula();
+	string getNombre();
+	string getNacionalidad();
+	string getResidencia();
+	int getTelefono();
+	int getNumeroHijos();
+	string getEstadoCivil();
+	int getHorasTrabajo();
+
+	// Setters
+	void setCedula(string perCedula);
+	void setNombre(string perNombre);
+	void setNacionalidad(string perNacionalidad);
+	void setResidencia(string perResidencia);
+	void setTelefono(int perTelefono);
+	void setNumeroHijos(int perNumeroHijos);
+	void setEstadoCivil(string perEstadoCivil);
+	void setHorasTrabajo(int perHorasTrabajo);
+
+	void MostrarPersona();
+	~Persona();
+};
+
